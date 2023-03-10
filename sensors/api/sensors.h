b67182e4e0199a20d9414b00b10a9b5176463e6a
xalbertoisorna <albertoisorna@xmos.com>
2023-03-10 11:05:23 +0000

    sensors

--- sensors/api/sensors.h
+++ sensors/api/sensors.h
@@ -1,28 +1,16 @@
 
 
-//  * Sensor abstraction layer.
+// -------------- Sensor abstraction layer. --------------
 
-typedef enum {
+typedef enum
+{
     CAMERA_IMX219,
-    CAMERA_OV2640,
-    CAMERA_OV3660,
-    CAMERA_OV5640,
-    CAMERA_OV7670,
-    CAMERA_NT99141,
     CAMERA_GC2145,
-    CAMERA_GC032A,
-    CAMERA_GC0308,
-    CAMERA_BF3005,
-    CAMERA_BF20A6,
-    CAMERA_SC101IOT,
-    CAMERA_SC030IOT,
-    CAMERA_SC031GS,
-    CAMERA_MODEL_MAX,
     CAMERA_NONE,
 } camera_model_t;
 
-
-typedef enum {
+typedef enum
+{
     PIXFORMAT_RGB565,    // 2BPP/RGB565
     PIXFORMAT_YUV422,    // 2BPP/YUV422
     PIXFORMAT_YUV420,    // 1.5BPP/YUV420
@@ -33,3 +21,40 @@ typedef enum {
     PIXFORMAT_RGB444,    // 3BP2P/RGB444
     PIXFORMAT_RGB555,    // 3BP2P/RGB555
 } pixformat_t;
+
+typedef enum
+{
+    FRAMESIZE_96X96,   // 96x96
+    FRAMESIZE_QQVGA,   // 160x120
+    FRAMESIZE_QCIF,    // 176x144
+    FRAMESIZE_HQVGA,   // 240x176
+    FRAMESIZE_240X240, // 240x240
+    FRAMESIZE_QVGA,    // 320x240
+    FRAMESIZE_CIF,     // 400x296
+    FRAMESIZE_HVGA,    // 480x320
+    FRAMESIZE_VGA,     // 640x480
+    FRAMESIZE_SVGA,    // 800x600
+    FRAMESIZE_XGA,     // 1024x768
+    FRAMESIZE_HD,      // 1280x720
+    FRAMESIZE_SXGA,    // 1280x1024
+    FRAMESIZE_UXGA,    // 1600x1200
+    // 3MP Sensors
+    FRAMESIZE_FHD,   // 1920x1080
+    FRAMESIZE_P_HD,  //  720x1280
+    FRAMESIZE_P_3MP, //  864x1536
+    FRAMESIZE_QXGA,  // 2048x1536
+    // 5MP Sensors
+    FRAMESIZE_QHD,   // 2560x1440
+    FRAMESIZE_WQXGA, // 2560x1600
+    FRAMESIZE_P_FHD, // 1080x1920
+    FRAMESIZE_QSXGA, // 2560x1920
+    FRAMESIZE_INVALID
+} framesize_t;
+
+typedef struct
+{
+    const camera_model_t camera_model;
+    const pixformat_t pix_format;
+    const framesize_t framse_size;
+} camera_sensor;
+S
