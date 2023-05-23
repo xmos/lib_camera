b6a82c8bef77650c9be0d8a1e22cc7894e610e9b
uvvpavel <paveluvarov@xmos.com>
2023-05-23 17:31:08 +0100

    cmake cleanup, changing xmos_cmake_toolchain to origin

--- examples/take_picture_dynamic_crop/take_picture_dynamic_crop.cmake
+++ examples/take_picture_dynamic_crop/take_picture_dynamic_crop.cmake
@@ -4,7 +4,6 @@
 
 # <--- Set the executable
 set(TARGET example_take_picture_crop)  
-set(DEST_FOLDER /mnt/c/Users/albertoisorna/exec/)  # to post build command
 
 file(GLOB_RECURSE APP_SOURCES ${CMAKE_CURRENT_LIST_DIR}/src/*.*c)
 set(APP_INCLUDES
@@ -62,20 +61,3 @@ target_compile_definitions(${TARGET} PRIVATE ${APP_COMPILE_DEFINITIONS})
 target_compile_options(${TARGET} PRIVATE ${APP_COMPILER_FLAGS})
 target_link_libraries(${TARGET} PUBLIC ${APP_COMMON_LINK_LIBRARIES})
 target_link_options(${TARGET} PRIVATE ${APP_LINK_OPTIONS})
-
-
-#**********************
-# Create run and debug targets
-#**********************
-create_run_target(${TARGET})
-create_debug_target(${TARGET})
-create_flash_app_target(${TARGET})
-create_install_target(${TARGET})
-
-# then custom command line to copy .xe file to custom folder
-if(EXISTS ${DEST_FOLDER})
-    add_custom_command(TARGET ${TARGET} POST_BUILD  
-        COMMAND ${CMAKE_COMMAND} -E copy
-        ${CMAKE_BINARY_DIR}/*.xe  ${DEST_FOLDER})
-endif()
-
