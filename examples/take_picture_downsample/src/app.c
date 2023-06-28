a9944a77de794fa8594f9005dd5bc1becce7af7d
uvvpavel <paveluvarov@xmos.com>
2023-06-28 10:42:13 +0100

    removing uncommented stuff

--- examples/take_picture_downsample/src/app.c
+++ examples/take_picture_downsample/src/app.c
@@ -11,9 +11,7 @@ void user_app()
   // Initialize camera api
   camera_init();
 
-  //int8_t image_buffer[APP_IMAGE_CHANNEL_COUNT][APP_IMAGE_HEIGHT_PIXELS][APP_IMAGE_WIDTH_PIXELS];
   int8_t image_buffer[APP_IMAGE_HEIGHT_PIXELS][APP_IMAGE_WIDTH_PIXELS][APP_IMAGE_CHANNEL_COUNT];
-  //uint8_t temp_buffer[APP_IMAGE_CHANNEL_COUNT][APP_IMAGE_HEIGHT_PIXELS][APP_IMAGE_WIDTH_PIXELS];
 
   // set the input image to 0
   memset(image_buffer, -128, sizeof(image_buffer));
@@ -32,38 +30,30 @@ void user_app()
   camera_stop();
   delay_milliseconds(100);
 
-  // convert to uint8 with right dimentions
   // convert to uint8
   vect_int8_to_uint8((uint8_t*) image_buffer,
-                     (int8_t*) image_buffer, 
+                     image_buffer,
                      sizeof(image_buffer));
-
-  //memcpy(temp_buffer, image_buffer, APP_IMAGE_CHANNEL_COUNT * APP_IMAGE_HEIGHT_PIXELS * APP_IMAGE_WIDTH_PIXELS * sizeof(uint8_t));
-  uint8_t * io_buff = (uint8_t *) &image_buffer[0][0][0];
-  // io_buff this will have [APP_IMAGE_HEIGHT_PIXELS][APP_IMAGE_WIDTH_PIXELS][APP_IMAGE_CHANNEL_COUNT] dimentions
   
   // apply gamma correction
   #if APPLY_GAMMA
-    isp_gamma((uint8_t *) io_buff,//&temp_buffer[0][0][0],
-                          &gamma_new[0], 
-                          APP_IMAGE_HEIGHT_PIXELS,
-                          APP_IMAGE_WIDTH_PIXELS,
-                          APP_IMAGE_CHANNEL_COUNT);
+    isp_gamma((uint8_t *) image_buffer,
+              &gamma_new[0], 
+              APP_IMAGE_HEIGHT_PIXELS,
+              APP_IMAGE_WIDTH_PIXELS,
+              APP_IMAGE_CHANNEL_COUNT);
   #endif
-  
-  /*swap_dimensions((uint8_t *) &temp_buffer[0][0][0], io_buff,
-                    APP_IMAGE_HEIGHT_PIXELS,
-                    APP_IMAGE_WIDTH_PIXELS,
-                    APP_IMAGE_CHANNEL_COUNT);*/
 
   // Write binary file
-  write_image_file("capture.bin", io_buff,
+  write_image_file("capture.bin",
+                    (uint8_t *) image_buffer,
                     APP_IMAGE_HEIGHT_PIXELS,
                     APP_IMAGE_WIDTH_PIXELS,
                     APP_IMAGE_CHANNEL_COUNT);
 
   // Write bmp file
-  write_bmp_file("capture.bmp", io_buff,
+  write_bmp_file("capture.bmp",
+                  (uint8_t *) image_buffer,
                   APP_IMAGE_HEIGHT_PIXELS,
                   APP_IMAGE_WIDTH_PIXELS,
                   APP_IMAGE_CHANNEL_COUNT);
