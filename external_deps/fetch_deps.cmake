7e71ac497507ce3e4b7f393e07d7920a6ef90579
xalbertoisorna” <albertoisorna@xmos.com>
2023-07-28 12:13:20 +0100

    git shalow to false

--- external_deps/fetch_deps.cmake
+++ external_deps/fetch_deps.cmake
@@ -4,7 +4,7 @@ FetchContent_Declare(
     fwk_io
     GIT_REPOSITORY https://github.com/xmos/fwk_io
     GIT_TAG        develop
-    GIT_SHALLOW    TRUE
+    GIT_SHALLOW    FALSE
     SOURCE_DIR     ${EXT_DEPS_DIR}/fwk_io
 )
 FetchContent_Populate(fwk_io)
