e49bfde0cefbad88323af4f10cd8622ca00b768c
xalbertoisorna” <albertoisorna@xmos.com>
2024-01-10 16:03:31 +0000

    moving sensors

--- lib_camera/lib_build_info.cmake
+++ lib_camera/lib_build_info.cmake
@@ -1,7 +1,7 @@
 set(LIB_NAME lib_camera)
 set(LIB_VERSION 0.2.1)
 set(LIB_DEPENDENT_MODULES i2c )
-set(LIB_INCLUDES api src/_sony_imx219)
+set(LIB_INCLUDES api src/sensors/_sony_imx219)
 set(LIB_COMPILER_FLAGS -Os -Wall -Werror -g -fxscope -mcmodel=large)
 
 XMOS_REGISTER_MODULE()
