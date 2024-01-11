7fa5ac5152e44f9cce7f93fb9d914778e35a3ac2
xalbertoisorna” <albertoisorna@xmos.com>
2024-01-11 15:42:34 +0000

    updating lib version

--- fwk_camera/lib_build_info.cmake
+++ fwk_camera/lib_build_info.cmake
@@ -1,5 +1,5 @@
 set(LIB_NAME fwk_camera) #TODO change name
-set(LIB_VERSION 0.2.1)
+set(LIB_VERSION 0.3.0)
 set(LIB_DEPENDENT_MODULES i2c )
 set(LIB_INCLUDES api src/sensors/_sony_imx219)
 set(LIB_COMPILER_FLAGS -Os -Wall -Werror -g -fxscope -mcmodel=large)
