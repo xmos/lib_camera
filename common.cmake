5ea2aed2dd17b373fd8698be95ae811123e2943d
xalbertoisorna” <albertoisorna@xmos.com>
2024-01-11 12:35:55 +0000

    fix common I2C

--- common.cmake
+++ common.cmake
@@ -8,21 +8,10 @@ set(repo_name fwk_camera)
 # Sandbox configuration
 set(XMOS_SANDBOX_DIR ${FWK_CAMERA_ROOT_DIR}/../)
 set(XMOS_DEP_DIR_${repo_name} ${FWK_CAMERA_ROOT_DIR})
-set(XMOS_DEP_DIR_i2c ${XMOS_SANDBOX_DIR}/fwk_io/modules)
 set(xscope_fileio "xscope_fileio(develop)")
 set(Unity "danielpieczko/lib_unity(2.5.2)")
 set(lib_camera ${repo_name})
 
-if(NOT EXISTS ${XMOS_SANDBOX_DIR}/fwk_io)
-    include(FetchContent)
-    FetchContent_Declare(
-        fwk_io
-        GIT_REPOSITORY git@github.com:danielpieczko/fwk_io
-        GIT_TAG xcommon_cmake
-        SOURCE_DIR ${XMOS_SANDBOX_DIR}/fwk_io
-    )
-    FetchContent_Populate(fwk_io)
-endif()
 
 # RAW format configuration
 set(CONFIG_RAW8 0x2A)
--- fwk_camera/lib_build_info.cmake
+++ fwk_camera/lib_build_info.cmake
@@ -4,4 +4,17 @@ set(LIB_DEPENDENT_MODULES i2c )
 set(LIB_INCLUDES api src/sensors/_sony_imx219)
 set(LIB_COMPILER_FLAGS -Os -Wall -Werror -g -fxscope -mcmodel=large)
 
+# I2C
+set(XMOS_DEP_DIR_i2c ${XMOS_SANDBOX_DIR}/fwk_io/modules)
+if(NOT EXISTS ${XMOS_SANDBOX_DIR}/fwk_io)
+    include(FetchContent)
+    FetchContent_Declare(
+        fwk_io
+        GIT_REPOSITORY git@github.com:danielpieczko/fwk_io
+        GIT_TAG xcommon_cmake
+        SOURCE_DIR ${XMOS_SANDBOX_DIR}/fwk_io
+    )
+    FetchContent_Populate(fwk_io)
+endif()
+
 XMOS_REGISTER_MODULE()
