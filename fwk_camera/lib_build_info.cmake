9bc71a369ec6e4ef3af913ef3568e3652c8af2c9
xalbertoisorna” <albertoisorna@xmos.com>
2024-01-11 12:07:12 +0000

    renaming lib name

--- fwk_camera/lib_build_info.cmake
+++ fwk_camera/lib_build_info.cmake
@@ -1,7 +1,4 @@
-set(FWK_CAMERA_ROOT_DIR ${CMAKE_SOURCE_DIR}/..) #TODO delete after rename
-include(${FWK_CAMERA_ROOT_DIR}/common.cmake)
-
-set(LIB_NAME ${lib_camera})
+set(LIB_NAME fwk_camera) #TODO change name
 set(LIB_VERSION 0.2.1)
 set(LIB_DEPENDENT_MODULES i2c )
 set(LIB_INCLUDES api src/sensors/_sony_imx219)
