24a6d59e556d5b74c7d495bfcccc2431f202f382
xalbertoisorna” <albertoisorna@xmos.com>
2024-01-15 10:06:45 +0000

    improving build examples

--- examples/build_examples.py
+++ examples/build_examples.py
@@ -1,21 +1,45 @@
 # Copyright 2024 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+# This file builds all examples in the examples folder within a single script.
+
+import shutil
 import subprocess
+from string import Template 
 
-commands = """
-cmake -G "Unix Makefiles" -S take_picture_downsample -B take_picture_downsample/build
-xmake -C take_picture_downsample/build
+generator = "Ninja" # or "Unix Makefiles"
+make = "ninja" # or "xmake"
 
-cmake -G "Unix Makefiles" -S take_picture_local -B take_picture_local/build
-xmake -C take_picture_local/build
+cmake_template = Template(f'cmake -G {generator} -S $src_dir -B $src_dir/build')
+xmake_template = Template(f'{make} -C $src_dir/build')
 
-cmake -G "Unix Makefiles" -S take_picture_raw -B take_picture_raw/build
-xmake -C take_picture_raw/build
-"""
 
-# Split the commands into a list of individual commands
-command_list = [command.strip() for command in commands.split('\n') if command.strip()]
+# List of folders
+dirs_srcs = [
+    "take_picture_downsample",
+    "take_picture_local",
+    "take_picture_raw"
+]
+
+# Store result codes
+result_codes = []
 
 # Run each command
-for command in command_list:
-    subprocess.run(command, shell=True)
+for dir in dirs_srcs:
+    # remove build and dir folders
+    shutil.rmtree(dir + "/build", ignore_errors=True)
+    shutil.rmtree(dir + "/bin", ignore_errors=True)
+    # cmake
+    cmake_cmd = cmake_template.substitute(src_dir=dir)
+    cmake_result = subprocess.run(cmake_cmd, shell=True)
+    # build
+    xmake_cmd = xmake_template.substitute(src_dir=dir)
+    xmake_result = subprocess.run(xmake_cmd, shell=True)
+    # results
+    assert(not cmake_result.returncode)
+    assert(not xmake_result.returncode)
+    result_codes.append(cmake_result.returncode)
+    result_codes.append(xmake_result.returncode)
+
+# Print result codes
+print("Result codes:", result_codes)
