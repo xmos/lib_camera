8fd34f834e2c64cf7825d3e926fa6bc1d2b5b33b
xalbertoisorna” <albertoisorna@xmos.com>
2024-02-23 14:47:44 +0000

    adding generic isp functions

---
+++ lib_camera/api/isp_functions.hpp
@@ -0,0 +1,66 @@
+// Copyright 2023-2024 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+#pragma once
+
+#include <cstdint>
+
+// -------------------------- Cpp --------------------------
+template<typename T>
+void isp_crop_template(
+    T* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    unsigned xu1,
+    unsigned yu1,
+    unsigned xu2,
+    unsigned yu2);
+
+template<typename T>
+void isp_resize_template(
+    const T* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    T* out_img,
+    const unsigned out_width,
+    const unsigned out_height);
+
+
+// -------------------------- C --------------------------
+extern "C" {
+    
+    void isp_crop_int8(
+        int8_t* img,
+        const unsigned in_width,
+        const unsigned in_height,
+        unsigned xu1,
+        unsigned yu1,
+        unsigned xu2,
+        unsigned yu2);
+
+    void isp_crop_uint8(
+        uint8_t* img,
+        const unsigned in_width,
+        const unsigned in_height,
+        unsigned xu1,
+        unsigned yu1,
+        unsigned xu2,
+        unsigned yu2);
+
+    void isp_resize_int8(
+        const int8_t* img,
+        const unsigned in_width,
+        const unsigned in_height,
+        int8_t* out_img,
+        const unsigned out_width,
+        const unsigned out_height);
+
+    void isp_resize_uint8(
+        const uint8_t* img,
+        const unsigned in_width,
+        const unsigned in_height,
+        uint8_t* out_img,
+        const unsigned out_width,
+        const unsigned out_height);
+
+} // extern "C"
---
+++ lib_camera/src/isp_functions.cpp
@@ -0,0 +1,133 @@
+// Copyright 2023-2024 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+#include <stdint.h>
+
+#include "isp_functions.hpp"
+
+// -------------------------------- Crop  --------------------------------
+template<typename T>
+void isp_crop_template(
+    T* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    unsigned xu1,
+    unsigned yu1,
+    unsigned xu2,
+    unsigned yu2) {
+
+    xu2 = (xu2 >= in_width) ? in_width - 1 : xu2;
+
+    unsigned out_width = xu2 - xu1;
+    xassert(out_width <= in_width);
+    xassert((yu2 - yu1) <= in_height);
+    unsigned in_row_len = in_width * 3;
+
+    unsigned out_index = 0;
+    unsigned in_index = yu1 * in_row_len + xu1 * 3;
+    unsigned out_row_len = out_width * 3;
+
+    for (unsigned i = yu1; i < yu2; i++) {
+        memmove(&img[out_index], &img[in_index], out_row_len);
+        in_index += in_row_len;
+        out_index += out_row_len;
+    }
+}
+
+// -------------------------------- Resize  --------------------------------
+static void xmodf(float a, int *b, float *c, int *bp)
+{
+  // split integer and decimal part
+  *b = (int)(a);
+  *c = a - *b;
+  // last operand for convinience 
+  *bp = *b + 1;
+}
+
+template<typename T>
+void isp_resize_template(
+    const T* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    T* out_img,
+    const unsigned out_width,
+    const unsigned out_height) {
+    const float x_ratio = ((in_width - 1) / (float)(out_width - 1));
+    const float y_ratio = ((in_height - 1) / (float)(out_height - 1));
+
+    unsigned x_l, y_l, x_h, y_h;
+    float xw, yw;
+    T a, b, c, d;
+
+    for (unsigned i = 0; i < out_height; i++) {
+        for (unsigned j = 0; j < out_width; j++) {
+            float incrx = (x_ratio * j);
+            float incry = (y_ratio * i);
+
+            xmodf(incrx, &x_l, &xw, &x_h);
+            xmodf(incry, &y_l, &yw, &y_h);
+
+            for (unsigned plane = 0; plane < 3; plane++) {
+                a = img[3 * in_width * y_l + 3 * x_l + plane];
+                b = img[3 * in_width * y_l + 3 * x_h + plane];
+                c = img[3 * in_width * y_h + 3 * x_l + plane];
+                d = img[3 * in_width * y_h + 3 * x_h + plane];
+
+                T pixel = (T)(a * (1 - xw) * (1 - yw) +
+                    b * xw * (1 - yw) +
+                    c * yw * (1 - xw) +
+                    d * xw * yw);
+
+                out_img[3 * out_width * i + 3 * j + plane] = pixel;
+            }
+        }
+    }
+}
+
+// -------------------------------- C Wrapper functions  --------------------------------
+
+// Extern C
+void isp_crop_int8(
+    int8_t* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    unsigned xu1,
+    unsigned yu1,
+    unsigned xu2,
+    unsigned yu2) {
+    isp_crop_template(img, in_width, in_height, xu1, yu1, xu2, yu2);
+}
+
+// Extern C
+void isp_crop_uint8(
+    uint8_t* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    unsigned xu1,
+    unsigned yu1,
+    unsigned xu2,
+    unsigned yu2) {
+    isp_crop_template(img, in_width, in_height, xu1, yu1, xu2, yu2);
+}
+
+// Extern C
+void isp_resize_int8(
+    const int8_t* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    int8_t* out_img,
+    const unsigned out_width,
+    const unsigned out_height) {
+    isp_resize_template(img, in_width, in_height, out_img, out_width, out_height);
+}
+
+// Extern C
+void isp_resize_uint8(
+    const uint8_t* img,
+    const unsigned in_width,
+    const unsigned in_height,
+    uint8_t* out_img,
+    const unsigned out_width,
+    const unsigned out_height) {
+    isp_resize_template(img, in_width, in_height, out_img, out_width, out_height);
+}
