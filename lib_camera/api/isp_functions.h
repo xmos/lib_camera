52152a8bd3cb533ac310e40f0471dea468017441
uvvpavel <paveluvarov@xmos.com>
2024-06-27 13:57:20 +0100

    adding comments

--- lib_camera/api/isp_functions.h
+++ lib_camera/api/isp_functions.h
@@ -46,27 +46,39 @@ void isp_resize_uint8(
 	const unsigned out_width,
 	const unsigned out_height);
 
+/** Will convert RGB image to the greyscale one.
+ *  Memory can be reused safely. n_pix must be a multiple of 4
+ */
 void isp_rgb_to_greyscale4(
 	int8_t * gs_img,
 	int8_t * img,
 	unsigned n_pix);
 
+/** Will convert RGB image to the greyscale one.
+ *  Memory can be reused safely. n_pix must be a multiple of 8
+ */
 void isp_rgb_to_greyscale8(
 	int8_t * gs_img,
 	int8_t * img,
 	unsigned n_pix);
 
+/** Will convert RGB image to the greyscale one.
+ *  Memory can be reused safely. n_pix must be a multiple of 16
+ */
 void isp_rgb_to_greyscale16(
 	int8_t * gs_img,
 	int8_t * img,
 	unsigned n_pix);
 
+/** Will convert RGB image to the greyscale one.
+ *  Memory can be reused safely. n_pix must be a multiple of 4
+ */
 inline void isp_rgb_to_greyscale(
 	int8_t * gs_img,
 	int8_t * img,
 	unsigned n_pix) {
-		isp_rgb_to_greyscale4(gs_img, img, n_pix);
-	}
+	isp_rgb_to_greyscale16(gs_img, img, n_pix);
+}
 
 #if defined(__XC__) || defined(__cplusplus)
 } // extern "C"
