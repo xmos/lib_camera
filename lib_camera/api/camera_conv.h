e077a5b85ab5becf2def46c9bff44f4fe22d51b4
xalbertoisorna <124703429+xalbertoisorna@users.noreply.github.com>
2025-08-01 11:43:31 +0100

    Update lib_camera/api/camera_conv.h

--- lib_camera/api/camera_conv.h
+++ lib_camera/api/camera_conv.h
@@ -117,7 +117,7 @@ void camera_int8_to_uint8(
  * This function requires input and output arrays to be double word aligned
  *
  * @param output    uint8_t output pointer
- * @param input     Array of int8_t that contains the input
+ * @param input     int8_t input pointer
  * @param length    Length of the input and output arrays
  */
 void camera_int8_to_uint8_fast(
